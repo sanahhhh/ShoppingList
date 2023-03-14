df703ab384ae2cbcbac0335c8fb7146751bbc226
sanahhhh <sasha.sasha.sasha199@mail.ru>
2023-03-14 13:51:05 +0400

    Realization of domain layers

--- app/src/main/java/com/example/shoppinglist/domain/AddShopItem.kt
+++ app/src/main/java/com/example/shoppinglist/domain/AddShopItem.kt
@@ -1,8 +1,8 @@
 package com.example.shoppinglist.domain
 
-class AddShopItem {
+class AddShopItem(private val shopListInterface: ShopListInterface) {
 
     fun addItem(shopItem: ShopItem) {
-
+        shopListInterface.addItem(shopItem)
     }
 }
